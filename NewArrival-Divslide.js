ef12c91b33fbbce8ff772871ac44f1726171edb0
Yashveer <yas041202@gmail.com>
2024-01-17 21:50:23 +0530

    New Arrival Section developed

---
+++ NewArrival-Divslide.js
@@ -0,0 +1,40 @@
+const divSlides = document.querySelectorAll(".divSlide");
+// console.log(divSlides);
+
+let counter1 = 0;
+
+divSlides.forEach(
+    (divSlide,index) =>{
+        divSlide.style.left = `${index*100}%`
+    }
+)
+
+const divGoprev = () =>{
+    if(counter1===0){
+        counter1=0;
+        slideDiv();
+    }
+    else{
+    counter1--;
+    slideDiv();
+    }
+}
+
+const divGonext = () =>{
+    if(counter1==divSlides.length-1){
+        counter1=divSlides.length-1;
+        slideDiv();
+    }
+    else{
+    counter1++;
+    slideDiv();
+    }
+}
+
+const slideDiv = () =>{
+    divSlides.forEach(
+        (divSlide) =>{
+            divSlide.style.transform = `translateX(-${counter1*100}%)`
+        }
+    )
+}
